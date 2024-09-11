2eb64358ff6e35c422a55eca54a1d03a851cecc2
Adam Booth <boothcamre@gmail.com>
2024-09-11 22:45:54 +0100

    (chore) update initial api structure

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,29 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { prettyJSON } from 'hono/pretty-json'
 
 const app = new Hono()
+app.get('/', (c) => c.text('test API'))
+app.use(prettyJSON())
+app.notFound((c) => c.json({ message: 'Not Found', ok: false }, 404))
 
-app.get('/', async (c) => {
-  const body = await c.req.json()
-  console.log(body)
-  return c.text('Hello Hono!')
-})
+type Bindings = {
+  USERNAME: string
+  PASSWORD: string
+}
+
+const api = new Hono<{ Bindings: Bindings }>()
+api.use('/heartbeat/*', cors())
+
+api.post(
+  '/heartbeat',
+  async (c) => {
+    const post = await c.req.json()
+    const ok = true
+    return c.json({ ok })
+  }
+)
+
+app.route('/api', api)
 
 export default app
